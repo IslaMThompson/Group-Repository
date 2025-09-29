e85a705507da3c7b19b9b6667f57d653f96f8b48
Isla Thompson <40711806@live.napier.ac.uk>
2025-09-29 03:46:40 +0100

    Created first class area

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+// For project classes when they get made
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Boo yah!");
+    }
+}
