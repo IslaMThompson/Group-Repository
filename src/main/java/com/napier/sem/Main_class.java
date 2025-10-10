1abd02fd711d0fdba5c87014011e6fe49667164d
Kai Bryan <bryan.07306@gmail.com>
2025-10-10 15:42:19 +0100

    Updated for project sql file

--- src/main/java/com/napier/sem/Main_class.java
+++ src/main/java/com/napier/sem/Main_class.java
@@ -46,7 +46,7 @@ public class Main_class
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
