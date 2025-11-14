c711c63dea170448b32bed1ebe175f0922faaa19
Kai Bryan <bryan.07306@gmail.com>
2025-11-14 01:12:54 +0000

    Added unit tests

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,60 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+    }
+
+    @Test
+    void getCityTestNull()
+    {
+        app.getCity(null);
+    }
+
+    @Test
+    void getCountryTestNull()
+    {
+        app.getCountry(null);
+    }
+
+    @Test
+    void displayCountryTestNull()
+    {
+        Country nullCountry = app.getCountry(null);
+        app.displayCountry(nullCountry);
+    }
+
+    @Test
+    void getCountriesByAreaTestNull()
+    {
+        app.getAllCountries();
+        app.getCountriesByContinent(null);
+        app.getCountriesByRegion(null);
+    }
+
+    @Test
+    void printCountriesTestNull()
+    {
+        app.printCountries(null);
+    }
+
+    @Test
+    void printCountriesTestEmpty()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        app.printCountries(countries);
+    }
+
+
+}
